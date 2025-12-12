49f063a759a1bab415f6c36cff069ecd4253bdcc
zac <zhuzhuyule@126.com>
2025-12-12 23:17:25 +0800

    feat: Update icon designs and colors, add hover effect to overlay icons, and remove VotypeTextLogo component.

--- src/components/icons/CancelIcon.tsx
+++ src/components/icons/CancelIcon.tsx
@@ -10,27 +10,23 @@ interface CancelIconProps {
 const CancelIcon: React.FC<CancelIconProps> = ({
   width = 24,
   height = 24,
-  color = "#FAA2CA",
+  color = "#767676",
   className = "",
 }) => {
   return (
     <svg
       width={width}
       height={height}
-      viewBox="0 0 24 24"
-      fill="none"
+      color={color}
+      viewBox="0 0 32 32"
       xmlns="http://www.w3.org/2000/svg"
       className={className}
     >
-      <g fill={color}>
-        <path d="m14.293 8.29297c.3905-.39052 1.0235-.39052 1.414 0s.3905 1.02354 0 1.41406l-5.99998 5.99997c-.39053.3906-1.02354.3906-1.41407 0-.39052-.3905-.39052-1.0235 0-1.414z" />
-        <path d="m8.29295 8.29297c.39053-.39052 1.02354-.39052 1.41407 0l5.99998 6.00003c.3905.3905.3905 1.0235 0 1.414-.3905.3906-1.0235.3906-1.414 0l-6.00005-5.99997c-.39052-.39052-.39052-1.02354 0-1.41406z" />
-        <path
-          d="m20 12c0-4.41828-3.5817-8-8-8-4.41828 0-8 3.58172-8 8 0 4.4183 3.58172 8 8 8 4.4183 0 8-3.5817 8-8zm2 0c0 5.5228-4.4772 10-10 10-5.52285 0-10-4.4772-10-10 0-5.52285 4.47715-10 10-10 5.5228 0 10 4.47715 10 10z"
-          opacity=".4"
-        />
-      </g>
+      <circle fill="none" stroke="currentColor" stroke-width="2" stroke-miterlimit="10" cx="16" cy="16" r="12" />
+      <path fill="none" stroke="currentColor" stroke-linejoin="round" stroke-linecap="round" stroke-width="2" d="m11.5 11.5 9 9m0-9-9 9" />
     </svg>
+
+
   );
 };
 
